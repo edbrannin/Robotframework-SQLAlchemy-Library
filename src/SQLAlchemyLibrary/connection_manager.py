dd44070b329e6b0f2112e09ae9f0afffeb284203
Ed Brannin <edbrannin@gmail.com>
2016-05-25 22:32:08 -0400

    Removed config-oriented Connect keywords until a we get good way to build URLs

--- src/SQLAlchemyLibrary/connection_manager.py
+++ src/SQLAlchemyLibrary/connection_manager.py
@@ -32,20 +32,12 @@ class ConnectionManager(object):
 
     def connect_to_database(self, url, echo=False, **kwargs):
         """
-        Loads the DB API 2.0 module given `dbapiModuleName` then uses it to
-        connect to the database using `dbName`, `dbUsername`, and `dbPassword`.
+        Connect to the given database URL with SQLAlchemy.
 
-        Optionally, you can specify a `dbConfigFile` wherein it will load the
-        default property values for `dbapiModuleName`, `dbName` `dbUsername`
-        and `dbPassword` (note: specifying `dbapiModuleName`, `dbName`
-        `dbUsername` or `dbPassword` directly will override the properties of
-        the same key in `dbConfigFile`). If no `dbConfigFile` is specified, it
-        defaults to `./resources/db.cfg`.
+        See also:
 
-        The `dbConfigFile` is useful if you don't want to check into your SCM
-        your database credentials.
-
-        See also http://docs.sqlalchemy.org/en/rel_1_0/core/engines.html#sqlalchemy.create_engine
+        - http://docs.sqlalchemy.org/en/latest/core/engines.html#database-urls
+        - http://docs.sqlalchemy.org/en/rel_1_0/core/engines.html#sqlalchemy.create_engine
 
         Example usage:
         | # Connect to an in-memory SQLite database |
@@ -53,29 +45,6 @@ class ConnectionManager(object):
 
         """
         self._engine = sqlalchemy.create_engine(url, echo=echo, **kwargs)
-        self._dbconnection = self._engine.connect()
-
-    def connect_to_database_from_config_file(self, config_file="sqlalchemy.ini"):
-        """
-        | # loads all property values from default.cfg |
-        | Connect To Database | dbConfigFile=default.cfg |
-
-        | # loads all property values from ./resources/db.cfg |
-        | Connect To Database |
-
-        | # uses explicit `dbapiModuleName` and `dbName` but uses the `dbUsername` and `dbPassword` in 'default.cfg' |
-        | Connect To Database | psycopg2 | my_db_test | dbConfigFile=default.cfg |
-
-        | # uses explicit `dbapiModuleName` and `dbName` but uses the `dbUsername` and `dbPassword` in './resources/db.cfg' |
-        | Connect To Database | psycopg2 | my_db_test |
-        """
-        config = ConfigParser.ConfigParser()
-        config.read([dbConfigFile])
-        # TODO
-        self._dbconnection.engine.connect()
-
-    def connect_to_database_from_config(self, config_dict, prefix="sqlalchemy.", **kwargs):
-        self._engine = sqlalchemy.engine_from_config(config_dict, prefix, **kwargs)
         self._dbconnection = self._engine.connect()
 
     @property
